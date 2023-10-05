Fixed multiple bugs relating to resetting the board and the monte_carlo_player average calculation

connect4.py
@@ -40,20 +40,39 @@ class Connect4:
                 return True
         return False
     
+    #Note: player symbols are hard coded
     def _is_finished(self):
         if any(self._has_won(player) for player in [1, -1]):
             return True
         if np.all(self.board != 0):
             return True
         return False
+    
+    #Note: player symbols are hard coded
+    def winner(self):
+        if self._has_won(1):
+            return 1
+        elif self._has_won(-1):
+            return -1
+        else:
+            return 0
 
-    def run(self, player1, player2):
-        self.reset()
+    def run_multiple_times(self, player1, player2, number_of_games):
+        results = []
+        for _ in range(0, number_of_games):
+            self.reset()
+            results.append(self.run_once(player1, player2))
+        return results
+
+
+    def run_once(self, player1, player2):
         moves = 0
+        if self._is_finished():
+            return self.winner(), moves
         while not self._is_finished():
             for player in [player1, player2]:
                 col = player.play(self.board)
                 self.play(col, player.symbol)
                 moves += 1
                 if self._is_finished():
-                    return player.symbol, moves
+                    return self.winner(), moves

monte_carlo_player.py
@@ -3,7 +3,7 @@ from connect4 import Connect4
 from random_player import RandomPlayer
 
 class MonteCarloPlayer():
-    def __init__(self,symbol, adversary_symbol, simulation_amount) -> None:
+    def __init__(self,symbol, adversary_symbol, simulation_amount):
         self.symbol = symbol
         self.adversary_symbol = adversary_symbol
         self.simulation_amount = simulation_amount
@@ -13,9 +13,15 @@ class MonteCarloPlayer():
         average_for_moves = {}
         for move in valid_columns:
             average_for_moves[move] = self._run_simulation(move, np.copy(board))
-        return self._largest_average(average_for_moves)
+        return self._best_possible_move(average_for_moves)
 
-    def _largest_average(self, average_for_moves):
+    def _best_possible_move(self, average_for_moves):
+        if self.symbol == 1:
+            return self._highest_average(average_for_moves)
+        else:
+            return self._lowest_average(average_for_moves)
+    
+    def _highest_average(self, average_for_moves):
         largest_average = -1
         best_move = -1
         for move in average_for_moves:
@@ -23,13 +29,22 @@ class MonteCarloPlayer():
                 best_move = move
                 largest_average = average_for_moves[move]
         return best_move
+    
+    def _lowest_average(self, average_for_moves):
+        lowest_average = 1
+        best_move = -1
+        for move in average_for_moves:
+            if average_for_moves[move] < lowest_average:
+                best_move = move
+                lowest_average = average_for_moves[move]
+        return best_move
         
     def _run_simulation(self, move, board_copy):
         game = Connect4()
-        game.set_board(board_copy)
-        game.play(move, self.symbol)
         averages = []
         for _ in range(0, self.simulation_amount):
-            averages.append(game.run(RandomPlayer(self.adversary_symbol), RandomPlayer(self.symbol))[0])
+            game.set_board(np.copy(board_copy))
+            game.play(move, self.symbol)
+            averages.append(game.run_once(RandomPlayer(self.adversary_symbol), RandomPlayer(self.symbol))[0])
         average_for_move = sum(averages)/len(averages)
         return average_for_move