9fd2b3226bd4ae8bfca962e77343adaf8f96e8e9
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-10-05 15:43:23 +0200

    Moved part1 and 2 to own folder

---
+++ part1_part2/main.py
@@ -0,0 +1,11 @@
+from random_player import RandomPlayer
+from monte_carlo_player import MonteCarloPlayer
+from distribution_of_moves import DistributionOfMoves
+
+if __name__ == "__main__":
+    # player1 = MonteCarloPlayer(1, -1, 10)
+    player2 = MonteCarloPlayer(1, -1, 2)
+    player1 = RandomPlayer(-1)
+    # player2 = RandomPlayer(-1)
+    runner = DistributionOfMoves(100, player1, player2)
+    runner.run()
