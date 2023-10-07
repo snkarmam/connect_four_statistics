4ccf853633ba998bafb2553c22b56860ebe0064f
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-10-07 17:38:36 +0200

    Fixed bug in part1/part2

--- part1_part2/distribution_of_moves.py
+++ part1_part2/distribution_of_moves.py
@@ -9,11 +9,12 @@ class DistributionOfMoves():
 
 
 	def run(self):
-		moves_distribution = {1: [], -1: []}
+		moves_distribution = {1: [], -1: [], 0: []}
 		
 		game = Connect4()
 		results = game.run_multiple_times(self.player1, self.player2, self.num_trials)
 		for tuple in results:
+			print(tuple)
 			moves_distribution[tuple[0]].append(tuple[1])
 		self._show_result(moves_distribution)
 		
@@ -21,9 +22,10 @@ class DistributionOfMoves():
 	def _show_result(self, moves_distribution):
 		print("Player 1 wins:", len(moves_distribution[1]))
 		print("Player 2 wins:", len(moves_distribution[-1]))
+		print("Draw: ", len(moves_distribution[0]))
 		
-		plt.hist(moves_distribution[1], alpha=0.5, label='Monte Carlo Player wins')
-		plt.hist(moves_distribution[-1], alpha=0.5, label='Player 2 wins')
+		plt.hist(moves_distribution[self.player1.symbol], alpha=0.5, label='Player with symbol '+ str(self.player1.symbol) + ' wins')
+		plt.hist(moves_distribution[self.player2.symbol], alpha=0.5, label='Player with symbol ' + str(self.player2.symbol) + ' wins')
 		plt.legend(loc='upper right')
 		plt.xlabel('Number of moves')
 		plt.ylabel('Frequency')
--- part1_part2/main.py
+++ part1_part2/main.py
@@ -4,8 +4,9 @@ from distribution_of_moves import DistributionOfMoves
 
 if __name__ == "__main__":
     # player1 = MonteCarloPlayer(1, -1, 10)
-    player2 = MonteCarloPlayer(1, -1, 2)
+    # player2 = MonteCarloPlayer(1, -1, 2)
     player1 = RandomPlayer(-1)
-    # player2 = RandomPlayer(-1)
+
+    player2 = RandomPlayer(1)
     runner = DistributionOfMoves(100, player1, player2)
     runner.run()
